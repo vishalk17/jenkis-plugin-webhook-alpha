4402afe68615da707901c75c13ca88f949082425
vishalk17 <vishalkapadi95@gmail.com>
2023-09-16 13:58:34 +0530

    Update webhook.java

--- webhook.java
+++ webhook.java
@@ -1,56 +1,63 @@
 import hudson.Extension;
-import hudson.model.Item;
 import hudson.model.ItemGroup;
-import hudson.model.Job;
 import hudson.model.TopLevelItem;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
 import jenkins.model.Jenkins;
-import org.kohsuke.github.GHEvent;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GHWebhook;
-import org.kohsuke.github.GitHub;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import java.io.IOException;
 import java.util.logging.Logger;
 
 @Extension
-public class GitWebhookTrigger extends Trigger {
+public class GitWebhookTrigger extends Trigger<WorkflowJob> {
 
     private static final Logger LOGGER = Logger.getLogger(GitWebhookTrigger.class.getName());
-
-    private String repositoryUrl;
-
-    public GitWebhookTrigger() {
-    }
-
-    public GitWebhookTrigger(String repositoryUrl) {
-        this.repositoryUrl = repositoryUrl;
-    }
 
     @Override
     public void run() {
         try {
-            // Parse the incoming webhook payload to extract relevant information
-            String payload = parseWebhookPayload();
+            // Get the Jenkins job associated with this trigger
+            WorkflowJob job = this.job;
 
-            // Handle the event based on the payload and its source
-            handleWebhookEvent(payload);
+            if (job == null) {
+                LOGGER.warning("No associated Jenkins job found.");
+                return;
+            }
+
+            // Check if the 'is_build_needed' environment variable is set to 'y'
+            if (isBuildNeeded(job)) {
+                // Trigger the Jenkins job
+                triggerJob(job);
+            } else {
+                LOGGER.info("Job '" + job.getName() + "' will not be triggered as 'is_build_needed' is not set.");
+            }
         } catch (IOException e) {
             LOGGER.error("Failed to trigger Jenkins job due to exception", e);
         }
     }
 
-    private String parseWebhookPayload() throws IOException {
-        // Implement logic to parse the incoming webhook payload
-        // You may need to inspect headers and request body to determine the source and payload format
-        // Return the parsed payload as a string
+    private boolean isBuildNeeded(WorkflowJob job) {
+        // Check if the 'is_build_needed' environment variable is set to 'y' for the specified Jenkins job
+        try {
+            String isBuildNeeded = job.getLastBuild().getEnvironment().get("is_build_needed");
+            return "y".equals(isBuildNeeded);
+        } catch (IOException | InterruptedException e) {
+            LOGGER.error("Error checking 'is_build_needed' environment variable: " + e.getMessage(), e);
+            return false;
+        }
     }
 
-    private void handleWebhookEvent(String payload) {
-        // Depending on the payload and its source, perform different actions
-        // You'll need to determine the source and event type based on the payload data
-        // Implement event handling logic here
+    private void triggerJob(WorkflowJob job) {
+        // Trigger the Jenkins job
+        try {
+            LOGGER.info("Triggering job: " + job.getName());
+            WorkflowRun run = job.scheduleBuild2(0);
+            LOGGER.info("Job triggered successfully. Build number: " + run.getNumber());
+        } catch (IOException e) {
+            LOGGER.error("Failed to trigger Jenkins job due to exception", e);
+        }
     }
 
     @Override
